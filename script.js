a8b8222f2c8dc60f57e97307cc372f5cff83eced
FlorianPx <florian.poux@hotmail.fr>
2020-10-01 16:48:56 +0200

    Add footer, contactForm and submit on script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const message = "Votre message est envoyé !";
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert(message);
+  });
