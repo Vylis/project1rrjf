1369c15d63265557624b4c590bae11bc4ab8fad0
vylis <vylis@users.noreply.github.com>
2020-10-06 14:19:33 +0200

    ok

--- carrousel.js
+++ carrousel.js
@@ -1,26 +1,24 @@
-
-
 /* CARROUSEL */
-
+ 
 const imgCarrousel = document.getElementsByClassName('carrousel-img');
 let imgActive = 0;
 imgCarrousel[imgActive].classList.add('show');
-
-
+ 
+ 
 for (let i = 1; i < imgCarrousel.length; i++) {
     imgCarrousel[i].classList.add('hidden');
 }
-
+ 
 // Clic bouton suivant
 document.querySelector('.carrousel-next').addEventListener('click', function(){
     next();
 });
-
+ 
 // Clic bouton précédent
 document.querySelector('.carrousel-prev').addEventListener('click', function(){
     prev();
 });
-
+ 
 // Fonction next
 const next = () => {
     imgCarrousel[imgActive].classList.remove('show');
@@ -32,9 +30,9 @@ const next = () => {
     imgCarrousel[imgActive].classList.remove('hidden');
     setTimeout(() => {
         imgCarrousel[imgActive].classList.add('show');
-    }, 100);
+    }, 50);
 };
-
+ 
 // Fonction prev
 const prev = () => {
     imgCarrousel[imgActive].classList.remove('show');
@@ -46,31 +44,34 @@ const prev = () => {
     imgCarrousel[imgActive].classList.remove('hidden');
     setTimeout(() => {
         imgCarrousel[imgActive].classList.add('show');
-    }, 100);
+    }, 50);
 };
-
-
+ 
+ 
 let interval;
 let playIcon = document.querySelector('.fa-play');
 let pauseIcon = document.querySelector('.fa-pause');
-
+ 
 // Clic bouton play
 document.getElementById('play').addEventListener('click', function(){
-    interval = setInterval(next, 5000);
-    playIcon.classList.add('active');
-    pauseIcon.classList.remove('active');
+    if (playIcon.classList.contains('active')) {
+        return;
+    }
+        playIcon.classList.add('active');
+        pauseIcon.classList.remove('active');
+        interval = setInterval(next, 5000);
 });
-
+ 
 // Clic bouton pause
 document.getElementById('pause').addEventListener('click', function(){
-    clearInterval(interval);
     playIcon.classList.remove('active');
     pauseIcon.classList.add('active');
+    clearInterval(interval); 
 });
-
+ 
 const autoPlay = () => {
     interval = setInterval(next, 5000);
     playIcon.classList.add('active');
 }
-
+ 
 autoPlay();
